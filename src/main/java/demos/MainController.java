8259e16e1e10ba81d72c86f20130fb802407227d
asy <asy@gmail.com>
2017-01-18 23:30:03 +0900

    webapp 폴더 파일 접근 테스트

--- src/main/java/demos/MainController.java
+++ src/main/java/demos/MainController.java
@@ -1,11 +1,21 @@
 package demos;
 
+import com.google.common.io.Files;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Set;
 
 
 /**
@@ -18,17 +28,27 @@ public class MainController {
 
     @RequestMapping("/")
     public String Index(Model model) throws Exception {
-
         logger.info("index.....");
-
         System.out.println("index");
-
         return "home";
 
     }
 
     @RequestMapping(value = "home.do")
-    public ModelAndView handleRequest() {
+    public ModelAndView handleRequest(HttpSession session,
+                                      HttpServletRequest request
+    ) throws IOException {
+        Set<String> resourcePaths = session.getServletContext().getResourcePaths("/");
+        System.out.println(resourcePaths);
+
+        Set<String> resourcePaths1 = request.getServletContext().getResourcePaths("/");
+        System.out.println(resourcePaths1);
+
+        String pdfPath = session.getServletContext().getRealPath("/email/test.html");
+        System.out.println(new File(pdfPath).exists());
+        System.out.println(Files.toString(new File(pdfPath), Charset.forName("UTF-8")));
+        System.out.println(IOUtils.toString(new FileInputStream(new File(pdfPath)), "UTF-8"));
+
         return new ModelAndView("home");
     }
 }
